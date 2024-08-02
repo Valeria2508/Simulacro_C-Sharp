05987ad3a9e1b8f51826d31ed6445f23fa5823f6
valeria2508 <valetqm2002@gmail.com>
2024-08-02 08:06:48 -0500

    feat: se agg constructor a clase vehicle

--- models/Vehicle.cs
+++ models/Vehicle.cs
@@ -16,5 +16,20 @@ namespace Simulacro_C_Shasp_VAle.models
         public byte PeopleCapacity { get; set; }
         public Driver Owner { get; set; }
 
+
+        //constructor
+        public Vehicle(int id, string placa, string tipo, string engineNumber, string serialNumber, byte peopleCapacity, Driver owner)
+        {
+            Id = id;
+            Placa = placa;
+            Tipo = tipo;
+            EngineNumber = engineNumber;
+            SerialNumber = serialNumber;
+            PeopleCapacity = peopleCapacity;
+            Owner = owner;
+        }
+
+        
+
     }
 }
